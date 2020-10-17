6420183b792381c18d4b5750bbb44c9310b057f7
Kompiang Gede Sukadharma <kompiang.sukadharma@gmail.com>
2020-10-17 18:41:26 +0800

    commit

--- faktorial.py
+++ faktorial.py
@@ -1,10 +1,25 @@
+def faktorial(angka):
+    hasil = 1
+    if angka > 0 :
+        while angka > 1:
+            hasil *= angka * (angka-1)
+            angka-=2
+        print("Hasilnya adalah :", hasil)
+    elif angka == 0:
+        hasil = 0
+        print("Hasilnya adalah :", hasil)
+    else:
+        print("Angka harus bilangan cacah")
+
 print("Aplikasi untuk menghitung hasil faktorial")
+print("Perhatikan berntuk di bawah")
+print("")
+print("-" * 59)
+print("| a! = 1 x 2 x ... x (a-1) x a,\
+ a himpunan bilangan cacah |")
+print("-" * 59)
+print("")
 
 angka = int(input("Masukkan angka yang diinginkan : "))
-hasil = 1
 
-while angka > 1:
-    hasil *= angka * (angka-1)
-    angka-=2
-
-print("Hasilnya adalah :", hasil)
+faktorial(angka)
