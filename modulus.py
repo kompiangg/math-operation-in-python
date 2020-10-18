c078c230de2424a91aa24b2dd016f6f52f78c473
Kompiang Gede Sukadharma <51501141+kompiangg@users.noreply.github.com>
2020-10-18 16:33:23 +0800

    Update modulus.py

--- modulus.py
+++ modulus.py
@@ -8,7 +8,7 @@ b = int(input("Masukkan b : "))
 
 if (a > 0 and b > 0) or (a < 0 and b < 0):
     hasil = a - int(a/b) * b
-elif (a == 0 and b > 0) or (a == 0 and b == 0):
+elif (a == 0 and b >= 0):
     hasil = 0
 elif a > 0 and b == 0:
     hasil = a
